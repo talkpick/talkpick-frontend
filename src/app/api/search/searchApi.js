2503fb40ac8235a1c743f377e0921e1e4e388acf
HaechangLee <112938092+HaechangLee@users.noreply.github.com>
2025-05-16 09:10:53 +0900

    ✨  Feat: API 호출 함수 정의

---
+++ src/app/api/search/searchApi.js
@@ -0,0 +1,11 @@
+import instance from '@/lib/axios';
+
+export const search = async (query, category, page, size) => {
+  const response = await instance.get(`/api/news/search?q=${query}&page=${page}&size=${size}`);
+  // TODO: API에 카테고리 항목 추가되면 아래로 수정
+  // const response = await instance.get(`/api/search?q=${query}&category=${category}&page=${page}&size=${size}`);
+  return response.data;
+};
+
+
+
