48093da75ee29d392dbc96ee00a0f338fea163a8
HaechangLee <112938092+HaechangLee@users.noreply.github.com>
2025-05-20 11:30:10 +0900

    🐛  Fix: api 요청 엔드포인트 수정 (#32)

--- src/app/api/news/[id]/newsDetailApi.js
+++ src/app/api/news/[id]/newsDetailApi.js
@@ -1,7 +1,7 @@
 import instance from '@/lib/axios';
 
 export const getNewsDetail = async (id) => {
-  const response = await instance.get(`/api/news/${id}`);
+  const response = await instance.get(`/api/public/news/${id}`);
   return response.data;
 };
 
