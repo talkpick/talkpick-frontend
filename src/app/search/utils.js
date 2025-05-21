5c699f600a167dcdd905d8aca46d0e3fc802d069
Yejeong, Ham <dev@thelightway.kr>
2025-05-21 11:49:39 +0900

    🔀 Revert: "Feature: #29 채팅방 컴포넌트 개발 (#34)" (#35)
    This reverts commit 8d3f9c872fecb5347ca8df618ebbb6a466f85786.

--- src/app/search/utils.js
+++ src/app/search/utils.js
@@ -16,8 +16,9 @@ export const formatDate = (dateString) => {
   const day = String(date.getDate()).padStart(2, '0');
   const hours = String(date.getHours()).padStart(2, '0');
   const minutes = String(date.getMinutes()).padStart(2, '0');
+  const seconds = String(date.getSeconds()).padStart(2, '0');
   
-  return `${year}-${month}-${day} ${hours}:${minutes}`;
+  return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
 };
 
 /**
--- src/contexts/AuthContext.js
+++ src/contexts/AuthContext.js
@@ -12,35 +12,29 @@ export const AuthContext = createContext();
 export function AuthProvider({ children }) {
   const router = useRouter();
   const [accessToken, setAccessToken] = useState(null);
-  const [nickname, setNickname] = useState(null);
 
   useEffect(() => {
     setAccessToken(localStorage.getItem('accessToken'));
-    setNickname(localStorage.getItem('nickname'));
   }, []);
 
   // login / logout 액션
   const login = useCallback(({ message, data }) => {
     localStorage.setItem('accessToken', data.accessToken);
     localStorage.setItem('refreshToken', data.refreshToken);
-    localStorage.setItem('nickname', data.nickname);
     setAccessToken(data.accessToken);
-    setNickname(data.nickname);
     router.push('/'); // 로그인 후 홈으로 이동
   }, [router]);
   
   const logout = useCallback(() => {
     localStorage.removeItem('accessToken');
     localStorage.removeItem('refreshToken');
-    localStorage.removeItem('nickname');
     setAccessToken(null);
-    setNickname(null);
     router.push('/'); // 로그아웃 후 홈으로 이동
   }, [router]);
 
   return (
     <AuthContext.Provider
-      value={{ accessToken, nickname, login, logout }}
+      value={{ accessToken, login, logout }}
     >
       {children}
     </AuthContext.Provider>
