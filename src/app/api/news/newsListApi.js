fdec7af851ac1525762a8805dc775fe48dcfd697
thelightway <dev@thelightway.kr>
2025-05-29 08:56:52 +0900

    🐛 Fix: instance is not defined 오류 수정

--- src/app/api/news/newsListApi.js
+++ src/app/api/news/newsListApi.js
@@ -1,12 +1,12 @@
 import refreshAxios from '@/lib/axios';
 
 export async function getLatestNews(page, size) {
-  const response = await instance.get(`/api/public/news/latest?page=${page}&size=${size}`);
+  const response = await refreshAxios.get(`/api/public/news/latest?page=${page}&size=${size}`);
   
   return response.data;
 }
 
 export async function getNewsByCategory(categoryId) {
-  const response = await instance.get(`/api/public/news/${categoryId}`);
+  const response = await refreshAxios.get(`/api/public/news/${categoryId}`);
   return response.data;
 } 
